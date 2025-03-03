b98edf89a4f31120d175a177aa05b8d2cb915d3f
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-03-03 12:59:30 +0530

    added

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,9 @@
-// cloudinaryConfig.js
 const cloudinary = require('cloudinary').v2;
 
-// Configure Cloudinary with your credentials
 cloudinary.config({
-  cloud_name: 'daczzyh7j',  // Replace with your cloud name
-  api_key: '554481782267576',       // Replace with your API key
-  api_secret: 'OKpOvrhWInE_PXuazta1QYqPt7Q', // Replace with your API secret
+  cloud_name: 'daczzyh7j',  
+  api_key: '554481782267576',       
+  api_secret: 'OKpOvrhWInE_PXuazta1QYqPt7Q', 
 });
 
 module.exports = cloudinary;
